d0b901f62cc57f0b11c5605a0a30fecb13f6be73
na_0_i <nayoi1oo5@gmail.com>
2023-03-29 17:21:01 +0900

    [style] globalstyle 변수 사용법 변경

--- app/styles/GlobalStyles.ts
+++ app/styles/GlobalStyles.ts
@@ -1,28 +1,14 @@
-import { createGlobalStyle } from 'styled-components';
-import reset from 'styled-reset';
+const globalStyles = {
+	colors: {
+		primary: '#51987F',
+		primary_light: 'E6EDEA',
+		primary_dark: '0F3D3E',
 
-const GlobalStyles = createGlobalStyle`
-    ${reset}
+		secondary: '#A5A5A5',
+		secondary_light: 'F7F7F7',
+		secondary_dark: '5F5F5F',
+		tertiary: '#FF9500',
+	},
+};
 
-	:root {
-    --color-primary: #51987F;
-    --color-primary-light: #E6EDEA;
-    --color-primary-dark: #0F3D3E;
-
-    --color-secondary: #A5A5A5;
-    --color-secondary-light: #F7F7F7;
-    --color-secondary-dark :#5F5F5F;
-
-    --font-size-8: 8px;
-		--font-size-10: 10px;
-		--font-size-12: 12px;
-		--font-size-14: 14px;
-		--font-size-16: 16px;
-		--font-size-18: 18px;
-		--font-size-20: 20px;
-		--font-size-22: 22px;
-    --font-size-24: 24px;
-  }
-`;
-
-export default GlobalStyles;
+export default globalStyles;
