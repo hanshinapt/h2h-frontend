7c418e2c404e13f12aa6fe17f0090f222eb5a9d6
na_0_i <nayoi1oo5@gmail.com>
2023-04-03 22:40:34 +0900

    [feat] Chip 컴포넌트 구현

---
+++ app/components/gameEntry/Chip.tsx
@@ -0,0 +1,27 @@
+// import { Text, View } from 'react-native';
+import styled from 'styled-components/native';
+
+interface ChipProps {
+	text: string;
+}
+
+const Chip = ({ text }: ChipProps) => {
+	return (
+		<ChipWrapper>
+			<ChipText>{text}</ChipText>
+		</ChipWrapper>
+	);
+};
+
+export default Chip;
+
+const ChipWrapper = styled.View`
+	background-color: white;
+	border-radius: 12px;
+	padding: 8px;
+	font-size: 16px;
+`;
+
+const ChipText = styled.Text`
+	color: #0f3d3e;
+`;
