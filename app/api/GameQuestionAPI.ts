1c2f2f15805655ae216da82d8daa96a8104ded06
na_0_i <nayoi1oo5@gmail.com>
2023-04-12 17:13:56 +0900

    [chore] gameQuestions 목업 데이터 생성

---
+++ app/api/GameQuestionAPI.ts
@@ -0,0 +1,23 @@
+const GameQuestionsTempData = {
+	deckId: '6abcdaaetawetawetawe',
+	title: '연인을 위한 대화카드',
+	questions: [
+		{
+			id: 0,
+			content:
+				'현재 나의 최대 관심사는 무엇인가요? 관심사에 어떻게 얼마나 투자하고 있으신가요?',
+		},
+		{
+			id: 1,
+			content: '현재 나의 최대 관심사는 무엇인가요?',
+		},
+		{
+			id: 2,
+			content: '관심사에 어떻게 얼마나 투자하고 있으신가요?',
+		},
+	],
+};
+
+export const getGameQuestions = () => {
+	return GameQuestionsTempData;
+};
