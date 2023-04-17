1b0e2111a602e94e472bbbf275ebd7a69c8159da
na_0_i <nayoi1oo5@gmail.com>
2023-04-17 22:18:08 +0900

    [feat] ImageComponent 구현

---
+++ app/components/common/Image.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components/native';
+import { Image, ImageSourcePropType, Dimensions } from 'react-native';
+
+interface ImageComponentProps {
+	imageUrl: ImageSourcePropType;
+	width?: number;
+	height?: number;
+}
+
+const ImageComponent = ({ imageUrl, width, height }: ImageComponentProps) => {
+	return (
+		<ImageWrapper>
+			<Image
+				source={imageUrl}
+				style={{ width: width ?? '100%', height: height ?? '100%' }}
+				resizeMode="cover"
+			/>
+		</ImageWrapper>
+	);
+};
+
+export default ImageComponent;
+
+const ImageWrapper = styled.View<{
+	width?: number | string;
+	height?: number | string;
+}>`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
