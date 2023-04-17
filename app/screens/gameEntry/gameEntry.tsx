3cb2c3cd98b7d2f6d4b90e54b4edb6f5e830fdc7
na_0_i <nayoi1oo5@gmail.com>
2023-04-17 22:18:24 +0900

    [feat] gameEntry 페이지 구현

--- app/screens/gameEntry/gameEntry.tsx
+++ app/screens/gameEntry/gameEntry.tsx
@@ -1,24 +1,54 @@
-import { getGameInfo } from '@/api/GameEntryAPI';
-import Button from '@/components/common/Button';
-import Chip from '@/components/gameEntry/Chip';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { StackParamList } from '@/App';
 import styled from 'styled-components/native';
+import { Text, ScrollView } from 'react-native';
+import { getGameInfo } from '@/api/GameEntryAPI';
+import ImageComponent from '@/components/common/Image';
+import ChipComponent from '@/components/gameEntry/Chip';
+import Icon from 'react-native-vector-icons/Ionicons';
+import IconButtonComponent from '@/components/common/IconButton';
 
-const GameEntry = () => {
+interface GameEntryProps {
+	navigation: NativeStackNavigationProp<StackParamList>;
+}
+
+const GameEntry = ({ navigation }: GameEntryProps) => {
 	const { title, tags, description } = getGameInfo();
 
 	return (
 		<GameEntryContainer>
 			<GameTitle>{title}</GameTitle>
-			<ChipsContainer>
-				{tags.map(({ name }) => (
-					<Chip text={`# ${name}`} />
-				))}
-			</ChipsContainer>
-			<GameDesc>{description}</GameDesc>
-			<ButtonContainer>
-				<Button text="뒤로가기" />
-				<Button text="게임 진행하기" />
-			</ButtonContainer>
+
+			<GameContentsWrapper>
+				<GameImageWrapper>
+					<ImageComponent imageUrl={require('@assets/love/love1.jpg')} />
+				</GameImageWrapper>
+				<GameChipsWrapper>
+					<ScrollView
+						horizontal
+						showsHorizontalScrollIndicator={false}
+						contentContainerStyle={{ gap: 16 }}>
+						{tags.map(({ id, name }) => (
+							<ChipComponent key={id} text={`# ${name}`} color="#51987F" />
+						))}
+					</ScrollView>
+				</GameChipsWrapper>
+				<GameDesc>{description}</GameDesc>
+			</GameContentsWrapper>
+
+			<ButtonsContainer>
+				<IconButtonComponent
+					bgColor="#CCCCCC"
+					onPress={() => navigation.goBack()}>
+					<Text>뒤로 가기</Text>
+					<Icon name="arrow-back-outline" />
+				</IconButtonComponent>
+				{/* 추후 navigate 경로 변경 */}
+				<IconButtonComponent onPress={() => navigation.navigate('Welcome')}>
+					<Text>게임 진행하기</Text>
+					<Icon name="play-outline" />
+				</IconButtonComponent>
+			</ButtonsContainer>
 		</GameEntryContainer>
 	);
 };
@@ -30,9 +60,10 @@ const GameEntryContainer = styled.View`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
+	justify-content: space-between;
 	background-color: #0f3d3e;
-	padding: 16px;
-	gap: 16px;
+	padding: 24px 0px;
+	gap: 24px;
 `;
 
 const GameTitle = styled.Text`
@@ -41,20 +72,34 @@ const GameTitle = styled.Text`
 	color: white;
 `;
 
-const ChipsContainer = styled.View`
-	display: flex;
-	flex-direction: row;
+const GameContentsWrapper = styled.View`
+	flex: 1;
+	align-items: center;
 	gap: 16px;
+	width: 100%;
+`;
+
+const GameImageWrapper = styled.View`
+	width: 100%;
+	height: 50%;
+`;
+
+const GameChipsWrapper = styled.View`
+	width: 100%;
+	align-items: center;
+	padding: 0 16px;
 `;
 
 const GameDesc = styled.Text`
 	font-size: 16px;
 	color: white;
+	padding: 0 16px;
 `;
 
-const ButtonContainer = styled.View`
-	display: flex;
-	width: 100%;
+const ButtonsContainer = styled.View`
 	flex-direction: row;
 	justify-content: space-around;
+	width: 100%;
+	padding: 0 16px;
+	gap: 16px;
 `;
