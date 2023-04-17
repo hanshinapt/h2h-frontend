8dd6a4a611a22784d200a5ee8b63f1e63ae5801b
na_0_i <nayoi1oo5@gmail.com>
2023-04-17 22:17:46 +0900

    [feat] ChipComponent 구현

--- app/components/gameEntry/Chip.tsx
+++ app/components/gameEntry/Chip.tsx
@@ -2,25 +2,31 @@ import styled from 'styled-components/native';
 
 interface ChipProps {
 	text: string;
+	color?: string;
+	bgColor?: string;
 }
 
-const Chip = ({ text }: ChipProps) => {
+const ChipComponent = ({ text, color, bgColor }: ChipProps) => {
 	return (
-		<ChipWrapper>
-			<ChipText>{text}</ChipText>
+		<ChipWrapper bgColor={bgColor}>
+			<ChipText color={color}>{text}</ChipText>
 		</ChipWrapper>
 	);
 };
 
-export default Chip;
+export default ChipComponent;
 
-const ChipWrapper = styled.View`
+const ChipWrapper = styled.View<{ bgColor?: string }>`
+	display: flex;
+	justify-content: center;
 	background-color: white;
 	border-radius: 12px;
 	padding: 8px;
 	font-size: 16px;
+	background-color: ${(props) => (props.bgColor ? props.bgColor : 'white')};
 `;
 
-const ChipText = styled.Text`
-	color: #0f3d3e;
+const ChipText = styled.Text<{ color?: string }>`
+	text-align: center;
+	color: ${(props) => props.color && props.color};
 `;
